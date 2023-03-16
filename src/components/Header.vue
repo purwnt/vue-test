ade808c7c8733e841d55147480c200a3e38b2b7e
Purwanto <purwanto1337@gmail.com>
2023-03-16 18:01:09 +0700

    feat: add header component

---
+++ src/components/Header.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+  name: 'Header',
+  props: {
+    heading: {
+      type: String,
+      required: true,
+    },
+    subheading: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="header">
+    <h1 class="header__heading">{{ heading }}</h1>
+    <h3 class="header__subheading">{{ subheading }}</h3>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.header {
+  &__heading {
+    font-size: 24px;
+    font-weight: normal;
+  }
+
+  &__subheading {
+    font-size: 30px;
+    font-weight: bold;
+    margin-top: 0px;
+  }
+}
+</style>
